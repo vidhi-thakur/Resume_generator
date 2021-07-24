8f29f631753444c042af2f8771a2525c0f31daa6
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-24 19:50:02 +0530

    updated homePage component

--- resume-generator-app/src/components/Header.js
+++ resume-generator-app/src/components/Header.js
@@ -2,22 +2,19 @@ import React from 'react'
 import "./Header.css"
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
 
 function Header() {
     return (
         <div className="header">
-            <Navbar collapseOnSelect expand="lg" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/">Resume Maker</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav navbarScroll" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto justify-content-center">
-                            <Nav.Link href="/login">Login</Nav.Link>
-                            <Nav.Link href="/signup">Signup</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
+            <Navbar collapseOnSelect expand="lg" variant="dark" className="w-10/12 mx-auto">
+                <Navbar.Brand href="/">Resume Maker</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav navbarScroll" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto justify-content-center">
+                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/signup">Signup</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         </div>
     )
--- resume-generator-app/src/pages/HomePage.js
+++ resume-generator-app/src/pages/HomePage.js
@@ -4,12 +4,16 @@ import emptyHomePage from "../images/emptyHomePage.svg"
 
 function HomePage() {
     return (
-        <div className="homePage">
-            <div className="">
-                <button>Create resume</button>
+        <div className="homePage flex flex-col justify-center items-center w-10/12 mx-auto">
+            <div className="pt-6 text-right w-full">
+                <button className="mx-auto hover:bg-indigo-600 hover:text-white p-2.5 border-2 border-indigo-600 rounded-full text-indigo-600 font-bold">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
+                    </svg>
+                </button>
             </div>
-            <img src={emptyHomePage} alt="HomePage-Logo" />
-            <h2>Nothing here</h2>
+            <img className="h-60 mb-8 mt-12" src={emptyHomePage} alt="HomePage-Logo" />
+            <h3>Nothing here...</h3>
         </div>
     )
 }
