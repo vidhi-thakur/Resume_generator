fd5ecd2994af2aa13061c9128c51b4aef84944cf
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-24 18:09:14 +0530

    updated homepage

--- resume-generator-app/src/pages/HomePage.js
+++ resume-generator-app/src/pages/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./HomePage.css"
+import emptyHomePage from "../images/emptyHomePage.svg"
 
 function HomePage() {
     return (
         <div className="homePage">
-            I am home page
+            <div className="">
+                <button>Create resume</button>
+            </div>
+            <img src={emptyHomePage} alt="HomePage-Logo" />
+            <h2>Nothing here</h2>
         </div>
     )
 }
