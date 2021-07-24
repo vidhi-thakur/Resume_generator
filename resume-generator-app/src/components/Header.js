be75e9cd5a21ed6968db49cd54954ad4c4de431a
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-24 14:40:39 +0530

    updated welcome page

--- resume-generator-app/src/components/Header.js
+++ resume-generator-app/src/components/Header.js
@@ -9,15 +9,12 @@ function Header() {
         <div className="header">
             <Navbar collapseOnSelect expand="lg" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Resume Generator</Navbar.Brand>
+                    <Navbar.Brand href="/">Resume Maker</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav navbarScroll" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto justify-content-center">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="#projects">Projects</Nav.Link>
-                            <Nav.Link href="#blogs">Blogs</Nav.Link>
-                            <Nav.Link href="#contact">Contact</Nav.Link>
+                            <Nav.Link href="/login">Login</Nav.Link>
+                            <Nav.Link href="/signup">Signup</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- resume-generator-app/src/pages/WelcomePage.js
+++ resume-generator-app/src/pages/WelcomePage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./WelcomePage.css"
+import welcomeLogo from "../images/welcomeLogo.svg"
 
 function WelcomePage() {
     return (
-        <div>
-            I am welcome page
+        <div className="welcomePage">
+            <div className="welcome__innerContainer -flex-col">
+                <img className="welcome__img" src={welcomeLogo} alt="Welcome-Logo" />
+                <h2>Welcome to Resume Maker</h2>
+                <p style={{width: "400px"}}>Generate beautiful resume and share it via shareable link or download generated resume.</p>
+            </div>
         </div>
     )
 }
