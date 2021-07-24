3b4ede47a7f7c774046f664e0bba2576a913401f
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-24 19:53:56 +0530

    updated homepage component
    added overlay to create resume button

--- resume-generator-app/src/pages/HomePage.js
+++ resume-generator-app/src/pages/HomePage.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import "./HomePage.css"
 import emptyHomePage from "../images/emptyHomePage.svg"
+import Tooltip from 'react-bootstrap/Tooltip'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 
 function HomePage() {
     return (
         <div className="homePage flex flex-col justify-center items-center w-10/12 mx-auto">
             <div className="pt-6 text-right w-full">
-                <button className="mx-auto hover:bg-indigo-600 hover:text-white p-2.5 border-2 border-indigo-600 rounded-full text-indigo-600 font-bold">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
-                    </svg>
-                </button>
+                <OverlayTrigger
+                    key="bottom"
+                    placement="bottom"
+                    overlay={
+                        <Tooltip id={`tooltip-bottom`}>
+                            Create resume
+                        </Tooltip>
+                    }
+                >
+                    <button className="mx-auto hover:bg-indigo-600 hover:text-white p-2.5 border-2 border-indigo-600 rounded-full text-indigo-600 font-bold">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
+                        </svg>
+                    </button>
+                </OverlayTrigger>
+
             </div>
             <img className="h-60 mb-8 mt-12" src={emptyHomePage} alt="HomePage-Logo" />
             <h3>Nothing here...</h3>
