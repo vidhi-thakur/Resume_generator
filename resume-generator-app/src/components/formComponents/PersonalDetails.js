36d2a8b0292f54e91178aedbfaf14955a4c86b82
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-27 01:16:33 +0530

    updated form component

--- resume-generator-app/src/components/formComponents/PersonalDetails.js
+++ resume-generator-app/src/components/formComponents/PersonalDetails.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import "./PersonalDetails.css"
 
-function PersonalDetails() {
+function PersonalDetails({ name, email, phoneNo, nextStep, handleChange }) {
+
+    const handleNext = (e) => {
+        e.preventDefault();
+        nextStep()
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+            <h2 className="text-indigo-600 mt-12 mb-8 text-center">Personal Details</h2>
+            <div className="grid place-items-center content-center">
+                <form className="flex flex-col justify-center w-10/12 mx-auto md:w-2/5 w-5/6">
+                    <label>Name</label>
+                    <input onChange={handleChange("userName")} value={name} className="px-4 py-2 mt-2.5 mb-4" type="text" placeholder="Eg: Jhonny Doe" required />
+                    <label>Email</label>
+                    <input onChange={handleChange("userEmail")} value={email} className="px-4 py-2 mt-2.5 mb-4" type="email" placeholder="Eg: Jhonnydoe@gmail.com" required />
+                    <label>Contant no.</label>
+                    <input onChange={handleChange("userPhoneNo")} value={phoneNo} className="px-4 py-2 mt-2.5 mb-12" placeholder="Eg: 9899545844 (optional)" />
+                    <button type="submit" onClick={handleNext} className="mx-auto hover:bg-indigo-600 hover:text-white py-1 px-2.5 border-2 border-indigo-600 text-indigo-600 text-xl font-bold">Next <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                    </svg></button>
+                </form>
+            </div>
+        </>
     )
 }
 
--- resume-generator-app/src/pages/CreateResumeForm.js
+++ resume-generator-app/src/pages/CreateResumeForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import "./CreateResumeForm.css"
 import PersonalDetails from "../components/formComponents/PersonalDetails"
+import ProfessionalDetails from '../components/formComponents/ProfessionalDetails'
 
 function CreateResumeForm() {
 
@@ -9,33 +10,49 @@ function CreateResumeForm() {
         userName: "",
         userEmail: "",
         userPhoneNo: "",
-        socialProfileName: "",
-        socialProfileLink: "",
+        socialProfileName: [],
+        socialProfileLink: [],
         skills: [],
-        jobTitle: "",
-        companyName: "",
-        jobDuration: "",
-        jobDescription: "",
-        projectTitle: "",
-        projectLink: "",
-        projectDuration: "",
-        projectDescription: "",
-        academicMilestone: "",
-        passoutYear: "",
-        schoolName: "",
+        jobTitle: [],
+        companyName: [],
+        jobDuration: [],
+        jobDescription: [],
+        projectTitle: [],
+        projectLink: [],
+        projectDuration: [],
+        projectDescription: [],
+        academicMilestone: [],
+        passoutYear: [],
+        schoolName: [],
         summary: "",
-        customTitle: "",
-        customLink: "",
-        customDescription: ""
+        customTitle: [],
+        customLink: [],
+        customDescription: []
     }
 
     const [formState, setFormState] = useState(initialState)
 
+    const nextStep = () => {
+        console.log("step before >>>>",formState.step)
+        setFormState({step: formState.step + 1})
+        console.log("step after >>>>",formState.step)
+    }
+
+    const prevStep = () => {
+        setFormState(formState.step - 1)
+    }
+
+    const handleChange = input => e => {
+        setFormState({[input]: e.target.value})
+    }
+
     switch (formState.step) {
         case 1:
-            return <PersonalDetails  />
+            return (<PersonalDetails name={formState.userName} email={formState.userEmail} phoneNo={formState.userPhoneNo} nextStep={nextStep} handleChange={handleChange} />)
+        case 2:
+            return (<ProfessionalDetails jobTitle={formState.jobTitle} companyName={formState.companyName} jobDuration={formState.jobDuration} jobDescription={formState.jobDescription} nextStep={nextStep} prevStep={prevStep} />)
         default:
-            return;
+            return <h1>Default page</h1>
 
     }
 }
