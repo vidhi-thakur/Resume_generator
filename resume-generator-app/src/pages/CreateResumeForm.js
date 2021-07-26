43a3a15903ffd4dd0989e905c8ccd57f3e82a171
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-07-26 18:32:25 +0530

    updated resume form component

--- resume-generator-app/src/pages/CreateResumeForm.js
+++ resume-generator-app/src/pages/CreateResumeForm.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import { useState } from 'react'
 import "./CreateResumeForm.css"
+import PersonalDetails from "../components/formComponents/PersonalDetails"
 
 function CreateResumeForm() {
-    return (
-        <div>
-            I am form page
-        </div>
-    )
+
+    const initialState = {
+        step: 1,
+        userName: "",
+        userEmail: "",
+        userPhoneNo: "",
+        socialProfileName: "",
+        socialProfileLink: "",
+        skills: [],
+        jobTitle: "",
+        companyName: "",
+        jobDuration: "",
+        jobDescription: "",
+        projectTitle: "",
+        projectLink: "",
+        projectDuration: "",
+        projectDescription: "",
+        academicMilestone: "",
+        passoutYear: "",
+        schoolName: "",
+        summary: "",
+        customTitle: "",
+        customLink: "",
+        customDescription: ""
+    }
+
+    const [formState, setFormState] = useState(initialState)
+
+    switch (formState.step) {
+        case 1:
+            return <PersonalDetails />
+        default:
+            return;
+
+    }
 }
 
 export default CreateResumeForm
